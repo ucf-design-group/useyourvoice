bc23adb23eaa250ce1b6682df9351b86955b8151
AJ Foster <web@aj-foster.com>
2013-11-18 14:30:35 -0500

    Change to IE 8 compatible version of jQuery.

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 		
 		<?php wp_footer(); ?>
 
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/main.js"></script>
 	</body>
 </html>
