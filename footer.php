e77faec39ffa0bf5fc1b8815e3afe293bc2363bc
AJ Foster <web@aj-foster.com>
2013-11-18 14:48:07 -0500

    Update mailing address in the footer.

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 			<!-- Set the backgrounds of each anchor with logo image sprite so it's one HTTP request, use Sass -->
 			<p>
 				© <?php echo date('Y'); ?> University of Central Florida Office of Student Involvement<br />
-				Student Union, Room 208, P.O. Box 163245, Orlando, FL 32816-3245<br />
+				Student Union, Room 208, 12715 Pegasus Drive, Orlando, FL 32816-3245<br />
 				email: osi@ucf.edu | phone: 407.823.6471 | fax: 407.823.5899<br />
 			</p>
 		</footer>
