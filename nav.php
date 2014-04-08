a7b509d0027203251358480ce4469f3b643ae806
John Costallos <jpcostallos@gmail.com>
2014-04-08 13:12:03 -0400

    Create new page templates add uyv logo

---
+++ nav.php
@@ -0,0 +1,7 @@
+<nav>
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="info.php">General Information</a></li>
+		<li><a href="locations">Locations</a></li>
+	</ul>
+</nav>
---
+++ nonuniversity-home.php
@@ -0,0 +1,19 @@
+<?php 
+/* Template Name: Non-University Home */
+get_header(); ?>
+<?php include 'nav.php'; ?>
+
+			<div class="content-area">
+				<div class="main"> 
+					<?php
+					while (have_posts()) {
+						the_post();
+						get_template_part( 'content', 'page' );
+					} ?>
+				</div>
+				<!-- <aside> -->
+					<!-- OPTIONAL -->
+				<!-- </aside> -->
+			</div>
+
+<?php get_footer(); ?>
