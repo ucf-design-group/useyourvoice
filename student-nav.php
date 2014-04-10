a084399775bca3254eb7ff8cb00084b74ef94b4d
John Costallos <jpcostallos@gmail.com>
2014-04-10 13:25:58 -0400

    Edit nav errors

--- student-nav.php
+++ student-nav.php
@@ -2,6 +2,6 @@
 	<ul>
 		<li><a href="http://sdesosiwebdev1.sdes.ucf.edu/test-area/home-2/ucf-student-guide">Home</a></li>
 		<li><a href="http://sdesosiwebdev1.sdes.ucf.edu/test-area/home-2/ucf-student-guide/student-general-info">General Information</a></li>
-		<li><a href="http://sdesosiwebdev1.sdes.ucf.edu/test-area/home-2/ucf-student-guide/student-locations‎">Locations</a></li>
+		<li><a href="http://sdesosiwebdev1.sdes.ucf.edu/test-area/home-2/ucf-student-guide/student-locations">Locations</a></li>
 	</ul>
 </nav>
