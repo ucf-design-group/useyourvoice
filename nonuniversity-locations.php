a8c3b9108136b26f09e98616899aa9096818c382
John Costallos <jpcostallos@gmail.com>
2014-04-10 13:26:43 -0400

    Create page templates for each website page

---
+++ nonuniversity-locations.php
@@ -0,0 +1,19 @@
+<?php 
+/* Template Name: Non-University Locations */
+get_header(); ?>
+<?php include 'nonuniversity-nav.php'; ?>
+
+			<div class="content-area">
+				<div class="main"> 
+					<?php
+					while (have_posts()) {
+						the_post();
+						get_template_part( 'content', 'page' );
+					} ?>
+				</div>
+				<!-- <aside> -->
+					<!-- OPTIONAL -->
+				<!-- </aside> -->
+			</div>
+
+<?php get_footer(); ?>
---
+++ student-general.php
@@ -0,0 +1,19 @@
+<?php 
+/* Template Name: Student General */
+get_header(); ?>
+<?php include 'student-nav.php'; ?>
+
+			<div class="content-area">
+				<div class="main"> 
+					<?php
+					while (have_posts()) {
+						the_post();
+						get_template_part( 'content', 'page' );
+					} ?>
+				</div>
+				<!-- <aside> -->
+					<!-- OPTIONAL -->
+				<!-- </aside> -->
+			</div>
+
+<?php get_footer(); ?>
